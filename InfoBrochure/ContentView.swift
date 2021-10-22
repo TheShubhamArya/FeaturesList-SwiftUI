5d841e9ba19c4ba1f6752dea996da1131659d108
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2021-10-22 17:33:13 -0500

    Add files via upload

--- InfoBrochure/ContentView.swift
+++ InfoBrochure/ContentView.swift
@@ -9,141 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView{
-            HStack{
+        NavigationView{
+            
+            ScrollView{
+                VStack{
+                    Text("Sortify 2.0 comes with some new exciting features that enhances your experience")
+                        .font(.system(size: 18))
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing])
+                    VStack(alignment: .leading, spacing: 20){
+                                                
+                        FeatureCell(color: .green, image: "note", headline: "Feature 1", subtitle: "Write a brief text describing the features of Feature 1 and how to to use.")
+                        
+                        FeatureCell(color: .orange, image: "pin", headline: "Feature 2", subtitle: "Write a brief text describing the features of Feature 2 and how to to use.")
+                        
+                        FeatureCell(color: .pink, image: "faceid", headline: "Feature 3", subtitle: "Write a brief text describing the features of Feature 3 and how to to use.")
+                        
+                        FeatureCell(color: .purple, image: "list.bullet.rectangle", headline: "Feature 4", subtitle: "Write a brief text describing the features of Feature 4 and how to to use.")
+                                                
+                        FeatureCell(color: .blue, image: "clock", headline: "Feature 5", subtitle: "Write a brief text describing the features of Feature 4 and how to to use.")
+                        
+                    }
+                    .padding()
+                    Spacer()
+                    
+                    Text("Short description about some other feature here that may not be as significant. Also can add a message from me.")
+                        .font(.system(size: 18))
+                        .padding([.leading, .bottom, .trailing])
+                        .multilineTextAlignment(.center)
+                    Text("Some more text can be added here. This is small in size and gray in color because it is insignificant.")
+                        .font(.system(size: 12))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.secondary)
+                        .padding(.horizontal)
+                    Spacer()
+                    
+                    NavigationLink(destination: SecondView()){
+                        HStack{
+                            Text("Next")
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.top)
+                }
+            }
+            .navigationBarTitle("What's New in 2.0?")
+            .padding()
+            .edgesIgnoringSafeArea(.bottom)
+            
+            .navigationBarItems(leading:
+                                    
                 Button(action: {
-                    /// Button Action goes here
-//                    if let topController = UIApplication.kTopViewController() {
-//                      topController.dismiss(animated: true)
-//                     }
-                }) {
+                /// Button Action goes here
+                //                    if let topController = UIApplication.kTopViewController() {
+                //                      topController.dismiss(animated: true)
+                //
+                })
+                {
                     Image(systemName: "xmark")
                         .foregroundColor(.red)
-                        .frame(width: 40, height: 40)
                 }
-                Spacer()
-            }
-            VStack{
-                Text("Sortify version 2.0")
-                    .font(.system(size: 30))
-                    .fontWeight(.heavy)
-                    .padding(.top)
-                Text("What's new in this version?")
-                    .font(.system(size: 24))
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
-                    .padding([.top, .leading, .trailing])
-                VStack(alignment: .leading, spacing: 20){
-                    
-                    HStack{
-                        Image(systemName: "note")
-                            .font(.system(size: 30))
-                            .foregroundColor(.green)
-                            .padding(.trailing)
-                            .font(.system(size: 20))
-                        
-                        VStack (alignment: .leading){
-                            Text("Feature 1")
-                                .bold()
-                                .font(.system(size: 20))
-                                .multilineTextAlignment(.leading)
-                            
-                            Text("Write a brief text describing the features of Feature 1 and how to to use.")
-                                .multilineTextAlignment(.leading)
-                            
-                        }
-                    }
-                    //.padding(.top)
-                    
-                    HStack{
-                        Image(systemName: "pin")
-                            .foregroundColor(.orange)
-                            .padding(.trailing)
-                            .font(.system(size: 30))
-                        
-                        VStack (alignment: .leading){
-                            Text("Feature 2")
-                                .bold()
-                                .font(.system(size: 20))
-                                .multilineTextAlignment(.leading)
-                            
-                            Text("Write a brief text describing the features of Feature 2 and how to to use.")
-                                .multilineTextAlignment(.leading)
-                            
-                        }
-                    }
-                    HStack{
-                        Image(systemName: "faceid")
-                            .font(.system(size: 30))
-                            .foregroundColor(.pink)
-                            .padding(.trailing)
-                        
-                        VStack (alignment: .leading){
-                            Text("Feature 3")
-                                .bold()
-                                .font(.system(size: 20))
-                                .multilineTextAlignment(.leading)
-                            
-                            Text("Write a brief text describing the features of Feature 3 and how to to use.")
-                                .multilineTextAlignment(.leading)
-                            
-                        }
-                    }
-                    
-                    HStack{
-                        Image(systemName: "list.bullet.rectangle")
-                            .font(.system(size: 27))
-                            .foregroundColor(.purple)
-                            .padding(.trailing)
-                        
-                        VStack (alignment: .leading){
-                            Text("Feature 4")
-                                .bold()
-                                .font(.system(size: 20))
-                                .multilineTextAlignment(.leading)
-                            
-                            Text("Write a brief text describing the features of Feature 4 and how to to use.")
-                                .multilineTextAlignment(.leading)
-                            
-                        }
-                    }
-                    
-                    HStack{
-                        Image(systemName: "clock")
-                            .font(.system(size: 30))
-                            .foregroundColor(.blue)
-                            .padding(.trailing)
-                        
-                        VStack (alignment: .leading){
-                            Text("Feature 5")
-                                .bold()
-                                .font(.system(size: 20))
-                                .multilineTextAlignment(.leading)
-                            
-                            Text("Write a brief text describing the features of Feature 4 and how to to use.")
-                                .multilineTextAlignment(.leading)
-                        }
-                        
-                    }
-                }
-                .padding()
-                Spacer()
-                
-                Text("Short description about some other feature here that may not be as significant. Also can add a message from me.")
-                    .font(.system(size: 18))
-                    .padding([.leading, .bottom, .trailing])
-                    .multilineTextAlignment(.center)
-                Text("Some more text can be added here. This is small in size and gray in color because it is insignificant.")
-                    .font(.system(size: 12))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.secondary)
-                    .padding(.horizontal)
-                
-                
-            }
+            )
         }
-        .padding()
-        .edgesIgnoringSafeArea(.bottom)
+    }
+    
+    struct FeatureCell: View {
+        let color: Color
+        let image: String
+        let headline: String
+        let subtitle: String
+        var body: some View {
+            HStack(alignment: .center) {
+                Image(systemName: image)
+                    .font(.system(size: 26,
+                                  weight: .bold,
+                                  design: .default))
+                    .foregroundColor(color)
+                    .padding(.trailing)
+                VStack(alignment: .leading) {
+                    Text(headline)
+                        .font(.system(size: 20,
+                                      weight: .semibold,
+                                      design: .default))
+                        .padding(.bottom, 1)
+                    Text(subtitle)
+                        .font(.system(size: 15, weight: .regular, design: .default))
+                        .foregroundColor(.gray)
+                    
+                }
+                Spacer()
+            }
+            .padding(5)
+        }
     }
 }
 
---
+++ InfoBrochure/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  InfoBrochure
+//
+//  Created by Shubham Arya on 1/17/21.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
